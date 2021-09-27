4e2d94ca26df860d12856ed2fd9ae09667d9c66f
noah figueras <noahfigueras@gmail.com>
2021-09-27 12:34:26 -0700

    Updates

---
+++ precache-manifest.f0d254380bcea06dac55c5bffca69748.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = [
+  {
+    "revision": "8495745c06abae186a18",
+    "url": "/website/static/css/main.34eb8a45.chunk.css"
+  },
+  {
+    "revision": "8495745c06abae186a18",
+    "url": "/website/static/js/main.8495745c.chunk.js"
+  },
+  {
+    "revision": "f510a1dc624b2dc05aac",
+    "url": "/website/static/js/1.f510a1dc.chunk.js"
+  },
+  {
+    "revision": "df8eefd1f2dbc5f3d248",
+    "url": "/website/static/js/runtime~main.df8eefd1.js"
+  },
+  {
+    "revision": "d8a86ceb9d31f6f91a6eec24facac178",
+    "url": "/website/static/media/home-background.d8a86ceb.jpg"
+  },
+  {
+    "revision": "777eacd1af5c9b53d1562b5986c708ab",
+    "url": "/website/static/media/Resume.777eacd1.pdf"
+  },
+  {
+    "revision": "b7ad2004fde930dc57968a6a4fcf0370",
+    "url": "/website/static/media/linkedin.b7ad2004.png"
+  },
+  {
+    "revision": "dac02c99359c9ea8e91c5a3e8574ce92",
+    "url": "/website/static/media/twitter.dac02c99.png"
+  },
+  {
+    "revision": "39ea5d3a2e047428fc8bec5b1c0609c9",
+    "url": "/website/static/media/noah-foto.39ea5d3a.png"
+  },
+  {
+    "revision": "1e3f4e0b519f5057795f186bd4224fb8",
+    "url": "/website/static/media/css3.1e3f4e0b.png"
+  },
+  {
+    "revision": "664a8c00ef526bf33edf55808601d461",
+    "url": "/website/static/media/html5.664a8c00.png"
+  },
+  {
+    "revision": "cfabce9481d64836519817f582993a7d",
+    "url": "/website/static/media/js.cfabce94.jpeg"
+  },
+  {
+    "revision": "80ce775771149d65c50594758d1c2ffd",
+    "url": "/website/static/media/react.80ce7757.png"
+  },
+  {
+    "revision": "3f5e5c1bc514004b51b14fd798e51051",
+    "url": "/website/static/media/mongodb.3f5e5c1b.png"
+  },
+  {
+    "revision": "56e64f3e884693bc6d065f9858741262",
+    "url": "/website/static/media/REST.56e64f3e.png"
+  },
+  {
+    "revision": "5c291d9f5e40b5813d32b61452196676",
+    "url": "/website/static/media/nodejs.5c291d9f.png"
+  },
+  {
+    "revision": "6ef42a8da411c603c041dbad54d6fc1d",
+    "url": "/website/static/media/sql.6ef42a8d.png"
+  },
+  {
+    "revision": "ce44abc28b5909022d42dff0b843bfa4",
+    "url": "/website/static/media/aws.ce44abc2.png"
+  },
+  {
+    "revision": "e1516f7582bdd2cf3d51419980caf174",
+    "url": "/website/static/media/git.e1516f75.png"
+  },
+  {
+    "revision": "eeb27d33423a48066983a9504960ec46",
+    "url": "/website/index.html"
+  }
+];
